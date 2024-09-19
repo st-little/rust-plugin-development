28bf372dad3184565c4f464393c9445c8630c891
Seiya Tashiro <seiya.tashiro@little-method.com>
2024-09-19 23:11:28 +0000

    #4 Develop a plugin to convert chat to kana

---
+++ Plugins/KanaChat/KanaChat.cs
@@ -0,0 +1,48 @@
+﻿//Reference: WanaKanaShaapu
+using ConVar;
+using WanaKanaShaapu;
+
+namespace Oxide.Plugins
+{
+    [Info("Kana Chat", "st-little", "0.1.0")]
+    [Description("Convert romaji typed in chat to kana.")]
+    public class KanaChat : RustPlugin
+    {
+        #region Oxide hooks
+
+        private object OnPlayerChat(BasePlayer player, string message, Chat.ChatChannel channel)
+        {
+            switch (channel)
+            {
+                case Chat.ChatChannel.Global:
+                    SendMessageToGlobalChat(ToKana(message));
+                    break;
+                case Chat.ChatChannel.Team:
+                    SendMessageToTeamChat(player, ToKana(message));
+                    break;
+            }
+            return false;
+        }
+
+        #endregion
+
+        private string ToKana(string message)
+        {
+            return $"{WanaKana.ToKana(message)}({message})";
+        }
+
+        private void SendMessageToGlobalChat(string message)
+        {
+            PrintToChat(message);
+        }
+
+        private void SendMessageToTeamChat(BasePlayer player, string message)
+        {
+            foreach (var member in player.Team.members)
+            {
+                BasePlayer basePlayer = RelationshipManager.FindByID(member);
+                PrintToChat(basePlayer, message);
+            }
+        }
+    }
+}
---
+++ Plugins/KanaChat/README.md
@@ -0,0 +1,15 @@
+# Kana Chat
+
+Japanese Romaji typed in chat will be converted to Japanese Hiragana using [WanaKana](https://wanakana.com/).
+
+## Installing Plugin
+
+1. Build [WanaKanaShaapu](https://github.com/kmoroz/WanaKanaShaapu/releases) and create a dll file.
+
+1. Copy the dll file to `RustDedicated_Data/Managed` directory on the server.
+
+1. Copy the cs file to `oxide/plugins` directory on the server.
+
+## Using Plugin
+
+1. Type Japanese romaji in Team Chat or Global Chat.
